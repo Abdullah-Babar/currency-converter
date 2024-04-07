53cf24e345c09b7bbd24012a1eef471a26f2a895
Abdullah Babar <abdullahbabar994@gmail.com>
2024-04-08 00:12:29 +0500

    My currency converter machine

---
+++ main.ts
@@ -0,0 +1,49 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const currency:any = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 278
+}
+
+console.log("\n Welcome! to my Currency Coverter Machine \n");
+
+
+let userAnswer = await inquirer.prompt([
+    {
+        name: "from",
+        type: "list",
+        message: "Enter Your From Currency",
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+
+    {
+        name: "to",
+        type: "list",
+        message: "Enter Your To Currency",
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+
+    {
+        name: "amount",
+        type: "number",
+        message: "Enter Your Amount"
+    }
+])
+
+let userFromCurrency = userAnswer.from
+let userToCurrency = userAnswer.to
+let fromAmount = currency[userFromCurrency]
+let toAmount = currency[userToCurrency]
+let amount = userAnswer.amount
+let baseAmount = amount / fromAmount
+let convertedAmount = baseAmount * toAmount
+console.log(convertedAmount);
+
+console.log("\nThanks! for using my machine ");
+
+
